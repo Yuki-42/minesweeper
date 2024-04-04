7cb1a3f690d6b1f1a53fec39ef9ce127939377a8
Yuki-42 <kylepannan@gmail.com>
2024-04-04 17:45:20 +1000

    Laid down base for api

--- server/main.py
+++ server/main.py
@@ -9,6 +9,29 @@ from fastapi import FastAPI
 
 # Local Imports
 from internals import Config, Database
-from internals.datatypes import User, Game, Board, Cell
+from internals.datatypes.db import User, Game
+from internals.logging import createLogger, SuppressedLoggerAdapter
 
-#
+# Create the FastAPI app
+app: FastAPI = FastAPI()
+
+# Create the internals objects
+config: Config = Config()
+database: Database = Database(config)
+logger: SuppressedLoggerAdapter = createLogger("Main", databaseConnection=database.connection)
+
+
+# Create the routes
+@app.get("/")
+async def root() -> dict[str, str]:
+    """
+    The root route.
+
+    Returns:
+        dict[str, str]: The message.
+    """
+    # Test the database
+    user: User = database.getUser(1)
+    game: Game = database.getGame(1)
+
+    return {"message": "Hello World"}
